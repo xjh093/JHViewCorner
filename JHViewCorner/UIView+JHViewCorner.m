3d1a28edcf4be38e998c76fcb7cd0efea7d0aba4
HaoCold <xjh093@126.com>
2018-05-25 15:01:27 +0800

    heart mask

--- JHViewCorner/UIView+JHViewCorner.m
+++ JHViewCorner/UIView+JHViewCorner.m
@@ -43,22 +43,13 @@
 #pragma mark - public
 
 - (void)jh_setCornerRadius:(CGFloat)radius color:(UIColor *)color rectCorner:(UIRectCorner)corner highlightedColor:(UIColor *)highlightedColor{
-    UIImage *image = [self imageWithCornerRadius:radius color:color rectCorner:corner];
+    UIImage *image = [self imageWithCornerRadius:radius color:color rectCorner:corner borderColor:nil borderWidth:0];
     if (image) {
-        UIImageView *maskView = [[UIImageView alloc] init];
-        maskView.frame = CGRectInset(self.bounds, -0.3, -0.3);
-        maskView.image = image;
-        [self addSubview:maskView];
-        [self setMaskView:maskView];
+        [self addMaskImage:image offset:-0.3];
 
-        if ([self colorJudge:highlightedColor]) {
-            UIImageView *highlightedImageView = [[UIImageView alloc] init];
-            highlightedImageView.frame = maskView.frame;
-            highlightedImageView.image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-            highlightedImageView.tintColor = highlightedColor;
-            highlightedImageView.hidden = YES;
-            [self addSubview:highlightedImageView];
-            [self setHighlightedMaskView:highlightedImageView];
+        UIImage *highlightedImage = [self imageWithCornerRadius:radius color:highlightedColor rectCorner:corner borderColor:nil borderWidth:0];
+        if (highlightedImage) {
+            [self addHighlightedMaskImage:highlightedImage offset:-0.3];
         }
     }
 }
@@ -66,21 +57,12 @@
 - (void)jh_setCornerRadius:(CGFloat)radius color:(UIColor *)color rectCorner:(UIRectCorner)corner borderColor:(UIColor *)borderColor borderWidth:(CGFloat)width highlightedColor:(UIColor *)highlightedColor{
     UIImage *image = [self imageWithCornerRadius:radius color:color rectCorner:corner borderColor:borderColor borderWidth:width];
     if (image) {
-        UIImageView *maskView = [[UIImageView alloc] init];
-        maskView.frame = CGRectInset(self.bounds, -0.3, -0.3);
-        maskView.image = image;
-        [self addSubview:maskView];
-        [self setMaskView:maskView];
+        [self addMaskImage:image offset:-0.3];
     }
     
     UIImage *highlightedImage = [self imageWithCornerRadius:radius color:highlightedColor rectCorner:corner borderColor:borderColor borderWidth:width];
     if (highlightedImage) {
-        UIImageView *highlightedImageView = [[UIImageView alloc] init];
-        highlightedImageView.frame = CGRectInset(self.bounds, -0.3, -0.3);
-        highlightedImageView.image = highlightedImage;
-        highlightedImageView.hidden = YES;
-        [self addSubview:highlightedImageView];
-        [self setHighlightedMaskView:highlightedImageView];
+        [self addHighlightedMaskImage:highlightedImage offset:-0.3];
     }
 }
 
@@ -92,32 +74,31 @@
     return [self getHighlightedMaskView];
 }
 
-#pragma mark - private
+- (void)jh_setHeartMask:(UIColor *)color highlightedColor:(UIColor *)highlightedColor{
+    UIImage *image = [self heartImageWithColor:color borderColor:nil borderWidth:0];
+    if (image) {
+        [self addMaskImage:image offset:0];
+        
+        UIImage *highlightedImage = [self heartImageWithColor:highlightedColor borderColor:nil borderWidth:0];
+        if (highlightedImage) {
+            [self addHighlightedMaskImage:highlightedImage offset:0];
+        }
+    }
+}
 
-- (UIImage *)imageWithCornerRadius:(CGFloat)radius color:(UIColor *)color rectCorner:(UIRectCorner)corner{
-    if (![self colorJudge:color]) {
-        return nil;
+- (void)jh_setHeartMask:(UIColor *)color borderColor:(UIColor *)borderColor borderWidth:(CGFloat)width highlightedColor:(UIColor *)highlightedColor{
+    UIImage *image = [self heartImageWithColor:color borderColor:borderColor borderWidth:width];
+    if (image) {
+        [self addMaskImage:image offset:0];
     }
     
-    CGRect bounds = self.bounds;
-    
-    UIGraphicsBeginImageContextWithOptions(bounds.size, NO, [UIScreen mainScreen].scale);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    [color set];
-    
-    // let rect a little big than bounds.
-    CGRect bigRect = CGRectInset(bounds, -0.3, -0.3);
-    CGRect smallRect = CGRectInset(bounds, 0.3, 0.3);
-    UIBezierPath *rect = [UIBezierPath bezierPathWithRect:bigRect];
-    UIBezierPath *round = [UIBezierPath bezierPathWithRoundedRect:smallRect byRoundingCorners:corner cornerRadii:CGSizeMake(radius, radius)];
-    [rect appendPath:round];
-    CGContextAddPath(context, rect.CGPath);
-    CGContextDrawPath(context, kCGPath);
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return image;
+    UIImage *highlightedImage = [self heartImageWithColor:highlightedColor borderColor:borderColor borderWidth:width];
+    if (highlightedImage) {
+        [self addHighlightedMaskImage:highlightedImage offset:0];
+    }
 }
+
+#pragma mark - private
 
 - (UIImage *)imageWithCornerRadius:(CGFloat)radius color:(UIColor *)color rectCorner:(UIRectCorner)corner borderColor:(UIColor *)borderColor borderWidth:(CGFloat)width{
 
@@ -156,6 +137,42 @@
     return image;
 }
 
+- (UIImage *)heartImageWithColor:(UIColor *)color borderColor:(UIColor *)borderColor borderWidth:(CGFloat)width{
+    if (![self colorJudge:color]) {
+        return nil;
+    }
+    
+    CGFloat offset1 = 0.5;
+    CGFloat offset2 = 2*offset1;
+    CGFloat w = CGRectGetWidth(self.bounds)-offset2*1.5;
+    
+    CGRect bounds = self.bounds;
+    CGSize size = CGSizeMake(bounds.size.width-offset1, bounds.size.height-offset1);
+    UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [color set];
+    
+    UIBezierPath *rect = [UIBezierPath bezierPathWithRect:bounds];
+    [self heartPath:w offset:offset1 rect:rect context:context];
+    
+#if 0
+    // TODO: border did not implement yet. 2018-05-25 14:56:59
+    // border
+    if ([self colorJudge:borderColor] && width > 0) {
+        [borderColor set];
+        
+        w -= width;
+        offset1 += width*0.5;
+        [self heartPath:w offset:offset1 rect:rect context:context];
+    }
+#endif
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
 - (BOOL)colorJudge:(UIColor *)color
 {
     if (color == nil) {
@@ -169,6 +186,50 @@
     }
     
     return YES;
+}
+
+- (void)heartPath:(CGFloat)w offset:(CGFloat)offset rect:(UIBezierPath *)rect context:(CGContextRef)context
+{
+    CGFloat radius = w*(7/24.0);
+    CGFloat point = radius + offset;
+    CGPoint leftPoint = CGPointMake(point,point);
+    CGPoint rightPoint = CGPointMake(w-point+offset*2,point);
+    
+    // clockwise 顺时针
+    UIBezierPath *leftArc = [UIBezierPath bezierPathWithArcCenter:leftPoint radius:radius startAngle:-M_PI_4 endAngle:M_PI_2+M_PI_4 clockwise:0];
+    
+    UIBezierPath *rightArc = [UIBezierPath bezierPathWithArcCenter:rightPoint radius:radius startAngle:-M_PI_2-M_PI_4 endAngle:M_PI_4 clockwise:1];
+    
+    UIBezierPath *line = [UIBezierPath bezierPath];
+    
+    [line moveToPoint:CGPointMake(w*0.5 + offset,w/12.0 + offset)];
+    [line addLineToPoint:CGPointMake(w/12.0 + offset, w*0.5 + offset)];
+    [line addLineToPoint:CGPointMake(w*0.5 + offset, w + offset)];
+    [line addLineToPoint:CGPointMake(w*11/12.0 + offset, w*0.5 + offset)];
+    
+    [rect appendPath:leftArc];
+    [rect appendPath:rightArc];
+    [rect appendPath:line];
+    
+    CGContextAddPath(context, rect.CGPath);
+    CGContextDrawPath(context, kCGPath);
+}
+
+- (void)addMaskImage:(UIImage *)image offset:(CGFloat)offset{
+    UIImageView *maskView = [[UIImageView alloc] init];
+    maskView.frame = CGRectInset(self.bounds, offset, offset);
+    maskView.image = image;
+    [self addSubview:maskView];
+    [self setMaskView:maskView];
+}
+
+- (void)addHighlightedMaskImage:(UIImage *)highlightedImage offset:(CGFloat)offset{
+    UIImageView *highlightedImageView = [[UIImageView alloc] init];
+    highlightedImageView.frame = CGRectInset(self.bounds, offset, offset);
+    highlightedImageView.image = highlightedImage;
+    highlightedImageView.hidden = YES;
+    [self addSubview:highlightedImageView];
+    [self setHighlightedMaskView:highlightedImageView];
 }
 
 - (void)setMaskView:(UIImageView *)imageView{
