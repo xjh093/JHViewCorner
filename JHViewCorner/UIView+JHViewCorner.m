2b19b255c92990bde9bdc14bbe8fa095daa9eb05
HaoCold <xjh093@126.com>
2023-04-13 16:04:07 +0800

    fix bug in iOS16

--- JHViewCorner/UIView+JHViewCorner.m
+++ JHViewCorner/UIView+JHViewCorner.m
@@ -42,9 +42,15 @@ static CGFloat _padding;
 
 @implementation UIView (JHViewCorner)
 
-+ (void)initialize{
-    _padding = 1.0/[UIScreen mainScreen].scale;
++ (void)load {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        _padding = 1.0/[UIScreen mainScreen].scale;
+    });
 }
+
+// + (void)initialize{
+//     _padding = 1.0/[UIScreen mainScreen].scale;
+// }
 
 #pragma mark - public
 
